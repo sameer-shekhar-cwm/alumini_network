567498693b2a6d335fd4ce6ae99bce4caf71e94d
Tanisha250 <tanisha.sirohi25@gmail.com>
2024-11-16 12:31:57 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Sample route for authentication
-router.get('/login', (req, res) => {
-  res.send('Login route');
-});
-
-module.exports = router;
